328b0ffecc162039d543cebff2d13dbd150798ee
Rishabh Verma <rishabhverma.2806@gmail.com>
2019-02-26 20:31:34 +1100

    Download working. Removed Font Picker

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pathPrefix: `/react-quotes`,
+    siteMetadata: {
+      title: `React Quotes`,
+      siteUrl: `www.rishverma.me/react-quotes`,
+      description: `Quotes Generator and Customizer using Gatsby`,
+    },
+  }
